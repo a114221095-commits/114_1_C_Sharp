a9ee78392c591b1daf3b13cf6b7cfbb3706135d0
a114221095-commits <a114221095@mail.shu.edu.tw>
2025-11-12 12:23:55 +0800

    11_12

---
+++ 11_12/Ending Balance/Ending Balance/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ending_Balance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            const decimal INTEREST_RATE = 0.005m;
+
+            decimal startingBalance;
+            int months;
+            int count = 1;
+            if (decimal.TryParse(startingBalTextBox.Text, out startingBalance))
+            {
+                if (int.TryParse(monthsTextBox.Text, out months))
+                {
+                    while (count <= months)
+                    {
+                        startingBalance *= (1 + INTEREST_RATE);
+                        detailListBox.Items.Add("第" + count + "個月結餘:" + startingBalance.ToString("c"));
+                        count++;
+                    }
+                    endingBalanceLabel.Text = startingBalance.ToString("c2");
+
+                }
+                else
+                {
+                    MessageBox.Show("請輸入有效月份數量(正整數)", "輸入錯誤");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("請輸入有效的起始餘額", "輸入錯誤");
+            }
+        }
+    
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the TextBoxes, the endingBalanceLabel control,
+            // and the ListBox.
+            startingBalTextBox.Text = "";
+            monthsTextBox.Text = "";
+            endingBalanceLabel.Text = "";
+            detailListBox.Items.Clear();
+
+            // Reset the focus.
+            startingBalTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
