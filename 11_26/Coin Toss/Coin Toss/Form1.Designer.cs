ec22b778173c450bf860fe3dc9c9238181ae51fa
a114221095-commits <a114221095@mail.shu.edu.tw>
2025-11-26 12:59:06 +0800

    11_26

---
+++ 11_26/Coin Toss/Coin Toss/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Coin_Toss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tailsPictureBox = new System.Windows.Forms.PictureBox();
+            this.headsPictureBox = new System.Windows.Forms.PictureBox();
+            this.tossButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.historyListBox = new System.Windows.Forms.ListBox();
+            this.headsLabel = new System.Windows.Forms.Label();
+            this.tailsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tailsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.headsPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tailsPictureBox
+            // 
+            this.tailsPictureBox.Image = global::Coin_Toss.Properties.Resources.Tails1;
+            this.tailsPictureBox.Location = new System.Drawing.Point(33, 12);
+            this.tailsPictureBox.Name = "tailsPictureBox";
+            this.tailsPictureBox.Size = new System.Drawing.Size(170, 170);
+            this.tailsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tailsPictureBox.TabIndex = 1;
+            this.tailsPictureBox.TabStop = false;
+            this.tailsPictureBox.Visible = false;
+            // 
+            // headsPictureBox
+            // 
+            this.headsPictureBox.Image = global::Coin_Toss.Properties.Resources.Heads1;
+            this.headsPictureBox.Location = new System.Drawing.Point(33, 12);
+            this.headsPictureBox.Name = "headsPictureBox";
+            this.headsPictureBox.Size = new System.Drawing.Size(170, 170);
+            this.headsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.headsPictureBox.TabIndex = 0;
+            this.headsPictureBox.TabStop = false;
+            // 
+            // tossButton
+            // 
+            this.tossButton.Location = new System.Drawing.Point(40, 194);
+            this.tossButton.Name = "tossButton";
+            this.tossButton.Size = new System.Drawing.Size(75, 23);
+            this.tossButton.TabIndex = 2;
+            this.tossButton.Text = "擲硬幣";
+            this.tossButton.UseVisualStyleBackColor = true;
+            this.tossButton.Click += new System.EventHandler(this.tossButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(121, 194);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "退出";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // historyListBox
+            // 
+            this.historyListBox.FormattingEnabled = true;
+            this.historyListBox.ItemHeight = 13;
+            this.historyListBox.Location = new System.Drawing.Point(220, 12);
+            this.historyListBox.Name = "historyListBox";
+            this.historyListBox.Size = new System.Drawing.Size(160, 174);
+            this.historyListBox.TabIndex = 4;
+            // 
+            // headsLabel
+            // 
+            this.headsLabel.AutoSize = true;
+            this.headsLabel.Location = new System.Drawing.Point(220, 194);
+            this.headsLabel.Name = "headsLabel";
+            this.headsLabel.Size = new System.Drawing.Size(48, 13);
+            this.headsLabel.TabIndex = 5;
+            this.headsLabel.Text = "Heads: 0";
+            // 
+            // tailsLabel
+            // 
+            this.tailsLabel.AutoSize = true;
+            this.tailsLabel.Location = new System.Drawing.Point(220, 214);
+            this.tailsLabel.Name = "tailsLabel";
+            this.tailsLabel.Size = new System.Drawing.Size(44, 13);
+            this.tailsLabel.TabIndex = 6;
+            this.tailsLabel.Text = "Tails: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 250);
+            this.Controls.Add(this.tailsLabel);
+            this.Controls.Add(this.headsLabel);
+            this.Controls.Add(this.historyListBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.tossButton);
+            this.Controls.Add(this.headsPictureBox);
+            this.Controls.Add(this.tailsPictureBox);
+            this.Name = "Form1";
+            this.Text = "擲硬幣";
+            ((System.ComponentModel.ISupportInitialize)(this.tailsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.headsPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox headsPictureBox;
+        private System.Windows.Forms.PictureBox tailsPictureBox;
+        private System.Windows.Forms.Button tossButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ListBox historyListBox;
+        private System.Windows.Forms.Label headsLabel;
+        private System.Windows.Forms.Label tailsLabel;
+    }
+}
---
+++ 11_26/Coin Toss/Coin Toss/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Coin_Toss
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();           // 單一 Random 實例
+        private int headsCount = 0;
+        private int tailsCount = 0;
+        private List<string> history = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateCounts();
+            UpdateHistoryListBox();
+        }
+
+        private void tossButton_Click(object sender, EventArgs e)
+        {
+            int sideUP = random.Next(2); // 產生 0 或 1
+
+            if (sideUP == 0)
+            {
+                // Display Heads
+                headsPictureBox.Visible = true;
+                tailsPictureBox.Visible = false;
+
+                headsCount++;
+                history.Add($"Heads - {DateTime.Now:T}");
+            }
+            else
+            {
+                // Display Tails
+                headsPictureBox.Visible = false;
+                tailsPictureBox.Visible = true;
+
+                tailsCount++;
+                history.Add($"Tails - {DateTime.Now:T}");
+            }
+
+            UpdateCounts();
+            UpdateHistoryListBox();
+        }
+
+        private void UpdateCounts()
+        {
+            headsLabel.Text = $"Heads: {headsCount}";
+            tailsLabel.Text = $"Tails: {tailsCount}";
+        }
+
+        private void UpdateHistoryListBox()
+        {
+            // 顯示最新的紀錄在最上方（反向迭代）
+            historyListBox.BeginUpdate();
+            historyListBox.Items.Clear();
+            for (int i = history.Count - 1; i >= 0; i--)
+            {
+                historyListBox.Items.Add(history[i]);
+            }
+            historyListBox.EndUpdate();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
